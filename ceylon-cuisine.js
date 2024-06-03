3e7482f1c6fd7b501ac984f09fdba43f1371c5f2
donMaleesha <lochanamt@gmail.com>
2024-06-03 13:22:42 +0530

    js

--- ceylon-cuisine.js
+++ ceylon-cuisine.js
@@ -1,20 +1,20 @@
-function updateWelcomeMessage() {
-    var currentTime = new Date();
-    var hours = currentTime.getHours();
-    var greeting;
-  
-    if (hours < 12) {
-      greeting = "Good morning";
-    } else if (hours < 18) {
-      greeting = "Good afternoon";
-    } else {
-      greeting = "Good evening";
-    }
-  
-    document.getElementById("welcomeMessage").textContent = greeting + ", welcome to our website!";
+document.addEventListener("DOMContentLoaded", function() {
+  function updateWelcomeMessage() {
+      var currentTime = new Date();
+      var hours = currentTime.getHours();
+      var greeting;
+
+      if (hours < 12) {
+          greeting = "Good morning";
+      } else if (hours < 18) {
+          greeting = "Good afternoon";
+      } else {
+          greeting = "Good evening";
+      }
+
+      document.getElementById("welcomeMessage").textContent = greeting + ", welcome to Ceylon Cuisine!";
   }
-  
+
   updateWelcomeMessage();
-  
   setInterval(updateWelcomeMessage, 60000);
-  
+});
