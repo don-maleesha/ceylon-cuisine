15453a55e2c9713f802766f01f57fdf564fe1b15
donMaleesha <lochanamt@gmail.com>
2024-06-03 13:23:29 +0530

    homePage

--- homePage.php
+++ homePage.php
@@ -1,7 +1,5 @@
 <?php
-
 include "dbconn.php";
-
 ?>
 
 <!doctype html>
@@ -16,48 +14,55 @@ include "dbconn.php";
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Satisfy&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
-  <script type="text/javascript" src="ceylon-cuisine.css"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap">
+  <script src="ceylon-cuisine.js"></script>
 </head>
 
 <body>
-  <header class="text-white">
+  <script src="./bootstrap-5.3.3-dist/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+
+  <header class="text-white align-items-center">
     <div class="container-fluid">
-      <div class="row align-items-center ">
-        <div class="col d-flex align-items-center">
-          <img src="./images/logo.png" alt="logo" class="logo-img">
+      <div class="row align-items-center">
+        <div class="col-3 d-flex align-items-center">
+          <img src="./images/logo.png" alt="logo" class="logo-img img-fluid">
         </div>
-        <div class="col d-flex align-items-center">
+        <div class="col-6 d-flex flex-column justify-content-center align-items-center">
           <h1 class="display-4 m-0 josefin-sans">Ceylon Cuisine</h1>
-          <!--<p class="tagline">Experience the Taste of Tradition</p>-->
+          <p class="tagline text-center">Experience the Taste of Tradition</p>
         </div>
-        <div class="col-auto text-end">
+        <div class="col-3 text-end">
           <nav class="navbar navbar-expand-md navbar-light">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse " id="navbarNav">
+            <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav">
-                <li class="nav-item  ubuntu-light-italic">
+                <li class="nav-item ubuntu-light-italic">
                   <a href="#" class="nav-link">About Us</a>
                 </li>
-                <li class="nav-item  ubuntu-light-italic">
+                <li class="nav-item ubuntu-light-italic">
                   <a href="#" class="nav-link">Contact</a>
                 </li>
                 <li class="nav-item">
-                  <a href="#" class="nav-link  ubuntu-light-italic">Recipe Categories</a>
+                  <a href="#" class="nav-link ubuntu-light-italic">Recipe Categories</a>
                 </li>
               </ul>
             </div>
           </nav>
         </div>
       </div>
+    </div>
   </header>
-  <div>
-    <h2 id="welcomeMessage">Welcome</h2>
-  </div>
+
+  <div class="container border p-4">
+    <div class="row">
+      <div class="col">
+        <h2 id="welcomeMessage" class="text-body-secondary"></h2>
+      </div>
+    </div>
   </div>
 
-</body>
+  </body>
 
 </html>
