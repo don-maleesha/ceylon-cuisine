4e7a0407a0c24a953d0f121b3950092c62c05c98
donMaleesha <lochanamt@gmail.com>
2024-10-12 01:40:53 +0530

    added content to recipe page

--- recipes.php
+++ recipes.php
@@ -12,6 +12,9 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Satisfy&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
   <script src="ceylon-cuisine.js"></script>
 </head>
 
@@ -55,7 +58,25 @@
     </div>
   </header>
 
-
+  <div class="container">
+    <div class="slider-container">
+      <div class="card-wrapper">
+        <div class="card">
+          <div class="image-box">
+            <img src="./images/th.jfif" alt="egg hoppers" class="img-fluid">
+          </div>
+          <div class="description">
+            <div class="recipe-name">
+              <h3>Egg Hoppers</h3>
+              <h4 class="ingrediants">Ingrediants: </h4>
+              <p>1 cup rice flour, 1 cup coconut milk, 1 egg, 1/2 tsp salt, 1/2 tsp sugar, 1/2 tsp yeast, 1/2 cup warm water, 1/2 tsp baking powder, 1/2 tsp baking soda, 1/2 tsp oil</p>
+              <h5 class="method">Method:</h5>
+              <p>Mix the rice flour, coconut milk, egg, salt, sugar, yeast, and warm water in a bowl. Let it ferment for 4 hours. Add baking powder, baking soda, and oil to the mixture. Pour the mixture into a hopper pan and cook for 2 minutes. Serve hot with sambol or curry.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <footer>
     <div class="container-fluid justify-content-center align-items-center mt-1">
       <div class="row">
