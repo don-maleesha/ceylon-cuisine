f420485378271cb15d93e8f43b0dabd5fa564005
donMaleesha <lochanamt@gmail.com>
2024-10-24 23:32:49 +0530

    finalized the about us page

--- aboutus.php
+++ aboutus.php
@@ -15,6 +15,9 @@ include "dbconn.php";
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Satisfy&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
   <script src="ceylon-cuisine.js"></script>
 </head>
 
@@ -62,21 +65,21 @@ include "dbconn.php";
   <div class="container rounded-3 col-6 d-flex flex-column justify-content-center align-items-center mt-5 mb-5 vh-100" id="about">
     <div class="row">
       <div class="col">
-        <h2 class="fw-semibold">About Us</h2>
-        <p class="fw-medium">Welcome to Ceylon Cuisine, your ultimate destination for authentic Ceylon cuisine! We are passionate about bringing the rich, diverse, and vibrant flavors of Sri Lankan food to your kitchen. Our mission is to preserve and share the traditional recipes that have been passed down through generations, offering a true taste of Ceylon's culinary heritage.</p>
+        <h2 class="h2 josefin-sans">About Us</h2>
+        <p class="fw-medium p">Welcome to Ceylon Cuisine, your ultimate destination for authentic Ceylon cuisine! We are passionate about bringing the rich, diverse, and vibrant flavors of Sri Lankan food to your kitchen. Our mission is to preserve and share the traditional recipes that have been passed down through generations, offering a true taste of Ceylon's culinary heritage.</p>
       </div>
       <div class="col">
-        <h2 class="fw-semibold">Our Story</h2>
-        <p class="fw-medium">Our journey began with a love for the aromatic spices, fresh ingredients, and unique cooking techniques that define Ceylon cuisine. We are a team of food enthusiasts, chefs, and storytellers who believe that food is more than just sustenance; it's a gateway to culture, history, and connection. Inspired by the bustling markets, fragrant spice gardens, and the warm hospitality of Sri Lanka, we set out to create a platform where anyone can experience the magic of Ceylon food.</p>
+        <h2 class="h2 josefin-sans">Our Story</h2>
+        <p class="fw-medium p">Our journey began with a love for the aromatic spices, fresh ingredients, and unique cooking techniques that define Ceylon cuisine. We are a team of food enthusiasts, chefs, and storytellers who believe that food is more than just sustenance; it's a gateway to culture, history, and connection. Inspired by the bustling markets, fragrant spice gardens, and the warm hospitality of Sri Lanka, we set out to create a platform where anyone can experience the magic of Sri Lankan food.</p>
       </div>
     </div>
     <div class="row">
-        <h2 class="fw-semibold">What We Offer</h2>
-        <p class="fw-medium">At Ceylon Cuisine, we offer a curated collection of traditional Sri Lankan recipes, each carefully tested and crafted to ensure authenticity and ease of preparation. Whether you're looking for a hearty curry, a tangy sambol, or a sweet treat, our recipes cover a wide range of dishes to suit every palate. Alongside our recipes, we provide detailed cooking tips, ingredient guides, and cultural insights to enhance your culinary journey.</p>
+        <h2 class="h2 josefin-sans">What We Offer</h2>
+        <p class="fw-medium p">At Ceylon Cuisine, we offer a curated collection of traditional Sri Lankan recipes, each carefully tested and crafted to ensure authenticity and ease of preparation. Whether you're looking for a hearty curry, a tangy sambol, or a sweet treat, our recipes cover a wide range of dishes to suit every palate. Alongside our recipes, we provide detailed cooking tips, ingredient guides, and cultural insights to enhance your culinary journey.</p>
     </div>
     <div class="row">
-        <h2 class="text-center fw-semibold">Join Us</h2>
-        <p class="text-center fw-medium">We invite you to join us on this culinary adventure. Whether you are a seasoned cook or a curious beginner, Ceylon Cuisine is here to guide you through the enchanting world of Sri Lankan food. Explore our recipes, share your creations, and become part of our vibrant community.
+        <h2 class="text-center h2 josefin-sans">Join Us</h2>
+        <p class="text-center fw-medium p">We invite you to join us on this culinary adventure. Whether you are a seasoned cook or a curious beginner, Ceylon Cuisine is here to guide you through the enchanting world of Sri Lankan food. Explore our recipes, share your creations, and become part of our vibrant community.
             ,<br>
             <br>Thank you for visiting Ceylon Cuisine. Let’s cook, share, and celebrate the flavors of Ceylon together!
         </p>
@@ -89,12 +92,12 @@ include "dbconn.php";
       <div class="row">
           <div class="col">
             <div class="">
-              <a href="#">Privacy Policy</a>
-              <a href="#">Terms of Conditions</a>
+              <a href="#" class="nav-link">Privacy Policy</a>
+              <a href="#" class="nav-link">Terms of Conditions</a>
             </div>
             <div class="col">
               <div class="mt-2">
-                <p>&copy; ceylon-cuisine 2024</p>
+                <p class="copy">&copy; ceylon-cuisine 2024</p>
               </div>
             </div>
           </div>
