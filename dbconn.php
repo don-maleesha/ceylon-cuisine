9822c81ef1c47f181f3c25117b68269881f57b11
donMaleesha <lochanamt@gmail.com>
2024-10-31 23:52:46 +0530

    changed db, cange receipe page styles

--- dbconn.php
+++ dbconn.php
@@ -3,7 +3,7 @@
     define("DB_HOST", "localhost");
     define("DB_USER", "root");
     define("DB_PASS", "");
-    define("DB_NAME", "ceylon-cuisine");
+    define("DB_NAME", "ceylon_cuisine");
 
     // Establish a connection to the MySQL database using the defined variables.
     $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
--- recipes.php
+++ recipes.php
@@ -229,12 +229,12 @@
       <div class="row">
           <div class="col">
             <div class="">
-              <a href="#">Privacy Policy</a>
-              <a href="#">Terms of Conditions</a>
+              <a href="#" class="nav-link">Privacy Policy</a>
+              <a href="#" class="nav-link">Terms of Conditions</a>
             </div>
             <div class="col">
               <div class="mt-2">
-                <p>&copy; ceylon-cuisine 2024</p>
+                <p class="copy">&copy; ceylon-cuisine 2024</p>
               </div>
             </div>
           </div>
